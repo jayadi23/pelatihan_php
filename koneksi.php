bc1a13235aaf225ae8a2e6bd257b7d94a8dad482
jayadi23 <irawanjayadi107@gmail.com>
2024-01-27 11:22:32 +0700

    jayadi

---
+++ koneksi.php
@@ -0,0 +1,8 @@
+<?php
+$conn = mysqli_connect("localhost", "root", "", "kampus");
+
+// if ($conn == true) {
+//     echo "Berhasil terhubung ke database.";
+// } else {
+//     echo "Gagal terhubung ke database. Error: ";
+// }
